3abbbe2fed42acf7eb94c44ae1dc29985dff323f
Gabriele Mongiano <hom3rster@gmail.com>
2024-03-10 11:49:06 +0100

    Improved documentation and implemented several updates in harpoon.el
    Changes:
    * Enhanced README.md file for better clarity and detailed understanding.
    * Added more functionality in harpoon.el such as functions for movement between bookmarks, loading and saving of harpoon data, and management of harpoon buffer.
    * Refined various existing functions for better performance.
    * Removed unnecessary comments and cleaned up the code for better readability.

--- README.md
+++ README.md
@@ -1,30 +1,79 @@
 # harpoon.el
 
-Harpoon (<https://github.com/ThePrimeagen/harpoon>) rudimentary replica for Emacs.
+[Harpoon](https://github.com/ThePrimeagen/harpoon "Harpoon") rudimentary replica for Emacs.
 
 ## Introduction
 
-**harpoon.el** is a quick project-based bookmarking system which simplifies navigating between commonly used files in a project. It leverages the built-in `project.el` and `bookmark.el` libraries.
+**harpoon.el** is a quick project-based bookmarking system which
+simplifies quick navigation between commonly used files in a
+project. It leverages the built-in `project.el` and `bookmark.el`
+libraries. An harpoon is simply an Emacs bookmark; therefore, you can
+set bookmarks for every buffer which has bookmark support (for
+example, shell buffers). Defining bookmark support for unsupported
+buffers should also be very simple, if needed.
 
-**Please note that this is under development and may not yet be thoroughly tested.**
-
-## Usage Instructions
+## Usage
 
 It's pretty simple:
 
 1. Enable `harpoon-minor-mode`.
-2. To set a bookmark, **from inside a known project**, invoke `harpoon-set`.
-3. Upon setting a bookmark, the `*harpoon*` buffer will appear. You can also invoke it manually with the `harpoon-buffer` command.
-4. You can now adjust the order of your harpoon bookmarks, delete any, or navigate as you please (press `<return>` while the point is on an entry). If you manually edit the list, remember to press `C-c C-c` to update the harpoon list and close the buffer (you could also manually kill the buffer).
-5. You can switch to the *n*-th bookmark by employing the `harpoon-jump-n` commands, where *n* is the bookmark position in the harpoon buffer.
+2. To set a bookmark, **from inside a known project**, invoke
+   `harpoon-set`.
+3. Upon setting a bookmark, the `*harpoon*` buffer will appear. You
+   can also invoke it manually with the `harpoon-buffer` command.
+4. You can now adjust the order of your harpoon bookmarks, delete any,
+   or navigate as you please (press `<return>` while the point is on
+   an entry). If you manually edit the list, remember to press `C-c
+   C-c` to update the harpoon list and close the buffer (you can also
+   manually kill the buffer).
+5. You can switch to the *n*-th bookmark by employing the
+   `harpoon-jump-n` commands, where *n* is the bookmark position in
+   the harpoon buffer.
 
-For instance, you can set keys combinations, e.g. `C-c h`, `C-c j`, `C-c k`, and `C-c l` (i.e. home row keys) to quickly jump to harpoons 1 through 4, respectively (see Example configuration below).
+For instance, you can set keys combinations, e.g. `C-c h`, `C-c j`,
+`C-c k`, and `C-c l` (i.e. right-handed home row keys) to quickly jump
+to harpoons 1 through 4 (see the example configuration below). You can
+also move forward and backward through your harpoon list using
+commands `harpoon-next` and `harpoon-prev`, respectively.
 
-You can use harpoon.el to swiftly switch between frequently accessed files in your project, increasing productivity and reducing navigation time.
+You can use `harpoon.el` to swiftly switch between frequently accessed
+files (but not only files) in your project, increasing productivity
+and reducing navigation time. You can read more about the concept of
+harpoon on the GitHub repository of the original implementation
+(<https://github.com/ThePrimeagen/harpoon>).
 
-You can read more about the concept of harpoon on the GitHub of the original implementation <https://github.com/ThePrimeagen/harpoon>.
+### Persisting harpoons across sessions
+
+Your project-specific harpoons are saved in `harpoon-list-file`
+(user-configurable, defaulting to a file named `harpoons` in your
+`user-emacs-directory`). Following the design of `bookmark.el`, your
+harpoons are saved to disk only before closing the Emacs session. If
+you want to force a save at any moment you can use `harpoon-file-save`
+(bound by default to `C-c C-s` while in the `harpoon` buffer).
+
+### Bookmarking files that are outside the current project
+
+You can achieve this by leveraging `project.el` builtin functionality:
+by defining `project-current-directory-override` as directory local
+variable you can tell Emacs to treat the current directory (and its
+sub-directories) as if it is part of another project. To do this, you
+can use interactive command `add-dir-local-variable`, like this:
+<kbd>M-x</kbd> `add-dir-local-variable` <kbd>RET</kbd> `nil` (apply to
+any mode) <kbd>RET</kbd> `project-current-directory-override`
+<kbd>RET</kbd> `/path/to/other/project` <kbd>RET</kbd>. Alternatively,
+you can manually edit the `.dir-locals` file of interest:
+
+```elisp
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((project-current-directory-override . "/path/to/other/project"))))
+```
 
 ## Example configuration
+
+The following is an example configuration that uses `C-c h` as prefix
+and then right hand home row keys to jump to harpoons 1 to 4.
 
 ```emacs-lisp
 (use-package harpoon
@@ -54,7 +103,8 @@ You can read more about the concept of harpoon on the GitHub of the original imp
 
 ### Using `elpaca`
 
-<https://github.com/progfolio/elpaca>
+If you don't want to manually clone the repository or handle the files
+you can use a package manager like [elpaca](https://github.com/progfolio/elpaca).
 
 ```emacs-lisp
 (use-package harpoon
@@ -63,6 +113,11 @@ You can read more about the concept of harpoon on the GitHub of the original imp
   )
 ```
 
-## Alternatives
+## Alternative implementation
 
-Users seeking the full Harpoon experience should probably check out the other Emacs implementation <https://github.com/otavioschwanck/harpoon.el>, which seems to be fully featured. This implementation is very simple and rudimentary. However, if you just need the basic functionality and usually rely on `project.el`, you can give it a try.
+Users seeking the full Harpoon experience should probably check out
+the other Emacs implementation
+<https://github.com/otavioschwanck/harpoon.el>, which seems to be
+fully featured. This implementation is very simple and
+rudimentary. However, if you just need the basic functionality and
+usually rely on `project.el`, you can give it a try.
--- harpoon.el
+++ harpoon.el
@@ -28,26 +28,44 @@
 ;;; Commentary:
 
 ;;; Code:
+
+;;;; Requirements
+
+(require 'cl-lib)
+(require 'bookmark)
+(require 'project)
+(require 'files)
+(require 'font-core)
+
+;;;; Customization
+
 (defgroup harpoon nil
-  "Harpoons quick bookmarking."
+  "Harpoon project-based quick bookmarking."
   :version "29.1"
   :group 'tools)
 
 (defcustom harpoon-buffer "*harpoon*"
-  "The name of the harpoon buffer"
+  "The name of the harpoon buffer."
   :type 'string
   :version "29.1"
   :group 'harpoon)
 
 (defcustom harpoon-list-file (locate-user-emacs-file "harpoons")
-  "The file where the harpoons for all the projects are saved"
+  "The file where the harpoons for all the projects are saved."
   :type 'file
   :version "29.1"
   :group 'harpoon
   )
 
+;;;; Variables
+
+(defvar harpoon-alist ()
+  "Harpoon list working memory.")
+
 (defvar harpoon-global-list (make-hash-table :test 'equal)
   "Global hash table mapping harpoons for different projects.")
+
+;;;; Functions
 
 (defun harpoon--update-global-list ()
   "Update the harpoon global list with the current project's harpoons."
@@ -62,64 +80,30 @@
     (let ((harpoons (gethash root harpoon-global-list)))
       (setq harpoon-alist harpoons))))
 
-(defvar harpoon-alist ()
-  "Harpoon list working memory.")
+(defun harpoon--buffer-name ()
+  "Return a string to be used as bookmark name.
 
-(defun harpoon--get-bookmark-name ()
+If the buffer is associated with a file, it will return the file
+path relative to `project-root'.  For all the other supported
+bookmark types (e.g. eshell) it will use the
+`bookmark-buffer-name' command."
   (when-let*
       ((proj (project-current))
-       (root (project-root proj))
-       (file (buffer-file-name)))
-    (file-relative-name file root)))
+       (root (project-root proj)))
+    (if (buffer-file-name)
+	(file-relative-name (buffer-file-name) root)
+      (bookmark-buffer-name))))
 
-(define-derived-mode harpoon-mode
-  fundamental-mode "Harpoon"
-  "Major mode for managing Harpoon bookmarks."
+(defun harpoon--position (harpoon)
+  "Return the 0-indexed current position of the harpoon named HARPOON.
 
-  (setq font-lock-defaults '((("^#.*" . 'font-lock-comment-face))))
-
-  (setq display-line-numbers t)
-  
-  ;; Keymaps
-  (let ((map harpoon-mode-map))
-    (define-key map (kbd "g") 'harpoon-buffer)
-    (define-key map (kbd "C-c C-c") 'harpoon--process-buffer-and-quit)
-    (define-key map (kbd "C-c C-s") 'harpoon-file-save)
-    (define-key map (kbd "<return>") 'find-file-at-point)))
-
-(defun harpoon-set ()
-  "Set an Harpoon for the current project.
-
-If the file is already harpoon'd just update the existing harpoon with the new location. Otherwise, append the new harpoon to the project's list of harpoons. If the current buffer does not belong to a project, fail silently."
-  (interactive)
-  (cl-assert harpoon-minor-mode nil "harpoon-minor-mode not enabled")
-  (when-let ((harpoon-name (harpoon--get-bookmark-name))
-	     (harpoon (funcall bookmark-make-record-function)))
-    (if-let ((existing (assoc harpoon-name harpoon-alist)))
-        (setcdr existing harpoon)
-      (progn 
-	(push (cons harpoon-name harpoon) harpoon-alist)
-	(setq harpoon-alist (nreverse harpoon-alist))))
-    (harpoon--update-global-list)
-    (harpoon-buffer)))
-
-(defun harpoon-jump (n)
-  "Generic function to jump to the n-th harpoon. Mind that `n' is 0-indexed.
-
-There are available aliases pre-defined like `harpoon-jump-1', `harpoon-jump-2', and so on, which are also 1-indexed to reflect the harpoon in the harpoon buffer."
-  (interactive)
-  (when-let ((bookmark (nth n harpoon-alist)))
-    (bookmark-jump bookmark)))
-
-;; Create harpoon-jump aliases. See `harpoon-jump' docstring.
-(dotimes (n 8)
-  (let ((func-name (intern (format "harpoon-jump-%d" (1+ n)))))
-    (defalias func-name `(lambda () (interactive) (harpoon-jump ,n))
-      (format "Jump to harpoon #%d." (1+ n)))))
-
+It returns nil if no one is matching."
+  (cl-position harpoon harpoon-alist
+	       :test (lambda (key element)
+		       (string= key (car element)))))
 
 (defun harpoon--alist-get-by-key (key)
-  "Get an entry from harpoon-alist by association key."
+  "Get an entry from `harpoon-alist' by KEY."
   (assoc key harpoon-alist))
 
 (defun harpoon--buffer-get-lines ()
@@ -128,21 +112,17 @@ There are available aliases pre-defined like `harpoon-jump-1', `harpoon-jump-2',
 If there is no Harpoon buffer return nil."
   (when-let* ((buffer (get-buffer harpoon-buffer)))
     (with-current-buffer buffer
-      (split-string (buffer-string) "\n" t))))
+      (split-string (buffer-substring-no-properties (point-min) (point-max)) "\n" t))))
 
 (defun harpoon--process-buffer ()
+  "Process the harpoon buffer to reflect deletions or changed harpoons order."
   (let* ((lines (harpoon--buffer-get-lines))
 	 (alist (mapcar #'harpoon--alist-get-by-key lines)))
     (setq harpoon-alist (delq nil alist))
     (harpoon--update-global-list)))
 
-(defun harpoon--process-buffer-and-quit ()
-  (interactive)
-  (when-let ((buffer (get-buffer harpoon-buffer)))
-    (harpoon--process-buffer)
-    (kill-buffer buffer)))
-
 (defun harpoon--insert-help (project-name-string)
+  "Insert help string into the harpoon buffer for project PROJECT-NAME-STRING."
   (let ((msg (format
 	      (string-join
 	       '(""
@@ -154,16 +134,124 @@ If there is no Harpoon buffer return nil."
 	      project-name-string)))
     (insert msg)))
 
+(defun harpoon--write-file (file data)
+  "Write the DATA to FILE to persist it across Emacs sessions."
+  (with-temp-file file
+    (prin1 data (current-buffer))))
+
+(defun harpoon--read-file (file)
+  "Read data from FILE."
+  (with-temp-buffer
+    (insert-file-contents file)
+    (goto-char (point-min))
+    (read (current-buffer))))
+
+(defun harpoon--file-load ()
+  "Update `harpoon-global-list' from the persistent file (`harpoon-list-file')."
+  (when (file-exists-p harpoon-list-file)
+    (let ((contents (harpoon--read-file harpoon-list-file)))
+      (setq harpoon-global-list contents)
+      (message "Harpoons loaded from file!"))))
+
+;;;; Commands
+
+(defun harpoon-set ()
+  "Set an Harpoon for the current project.
+
+If the file is already harpoon'd just update the existing harpoon
+with the new location.  Otherwise, append the new harpoon to the
+project's list of harpoons.  If the current buffer does not belong
+to a project, fail silently."
+  (interactive)
+  (cl-assert harpoon-minor-mode nil "harpoon-minor-mode not enabled")
+  (when-let ((harpoon-name (harpoon--buffer-name))
+	     (harpoon (funcall bookmark-make-record-function)))
+    (if-let ((existing (assoc harpoon-name harpoon-alist)))
+        (setcdr existing harpoon)
+      (progn
+	(push (cons harpoon-name harpoon) harpoon-alist)
+	(setq harpoon-alist (nreverse harpoon-alist))))
+    (harpoon--update-global-list)
+    (harpoon-buffer)))
+
+(defun harpoon-step (&optional backward)
+  "Move to the next harpoon, or to the previous harpoon if BACKWARD is not nil."
+  (when-let* ((name (harpoon--buffer-name))
+	      (index (harpoon--position name)))
+    (harpoon-jump (if backward (1- index) (1+ index)))))
+
+(defun harpoon-next ()
+  "Move to the next harpoon in the list."
+  (interactive)
+  (harpoon-step))
+
+(defun harpoon-prev ()
+  "Move to the previous harpoon in the list."
+  (interactive)
+  (harpoon-step t))
+
+(defun harpoon-jump (n)
+  "Generic function to jump to the N-th harpoon in the harpoon list.
+Mind that N is 0-indexed.
+
+There are available aliases pre-defined like `harpoon-jump-1',
+`harpoon-jump-2', and so on, which are also 1-indexed to reflect
+the harpoon in the harpoon buffer."
+  (interactive)
+  (when-let ((bookmark (nth n harpoon-alist)))
+    (bookmark-jump bookmark)))
+
+;; Function factory to create harpoon-jump aliases. See `harpoon-jump' docstring.
+(dotimes (n 8)
+  (let ((func-name (intern (format "harpoon-jump-%d" (1+ n)))))
+    (defalias func-name `(lambda () (interactive) (harpoon-jump ,n))
+      (format "Jump to harpoon #%d." (1+ n)))))
+
+
+(define-derived-mode harpoon-mode
+  fundamental-mode "Harpoon"
+  "Major mode for managing Harpoon bookmarks."
+
+  (setq-local font-lock-defaults '((("^#.*" . 'font-lock-comment-face))))
+
+  (setq display-line-numbers t)
+  
+  ;; Keymaps
+  (let ((map harpoon-mode-map))
+    (define-key map (kbd "g") 'harpoon-buffer)
+    (define-key map (kbd "C-c C-c") 'harpoon-process-buffer-and-quit)
+    (define-key map (kbd "C-c C-s") 'harpoon-file-save)))
+
+;;;###autoload
+(define-minor-mode harpoon-minor-mode
+  "A minor mode for using harpoon with Emacs."
+  :lighter " Hp"
+  :keymap (let ((map (make-sparse-keymap)))
+	    map)
+  :global t
+  (if harpoon-minor-mode
+      (progn
+	(harpoon--file-load)
+	(add-hook 'buffer-list-update-hook #'harpoon--retrieve)
+	(add-hook 'kill-emacs-hook #'harpoon-file-save))
+    (progn
+      (harpoon-file-save)
+      (remove-hook 'buffer-list-update-hook #'harpoon--retrieve)
+      (remove-hook 'kill-emacs-hook #'harpoon-file-save))))
+
+
 (defun harpoon-buffer ()
   "Invoke the `harpoon' buffer.
 
-From the `harpoon' buffer you can rearrange and/or delete harpoons by swapping or deleting lines, respectively, using standard Emacs text motions."
+From the `harpoon' buffer you can rearrange and/or delete
+harpoons by swapping or deleting lines, respectively, using
+standard Emacs text motions."
   (interactive)
   (cl-assert harpoon-minor-mode nil "harpoon-minor-mode not enabled")
   (if-let ((proj (project-current))
 	   (buffer (get-buffer-create harpoon-buffer))
 	   (files-list (mapcar #'car harpoon-alist)))
-      (progn 
+      (progn
 	(with-current-buffer buffer
 	  (erase-buffer)
 	  (harpoon-mode)
@@ -172,19 +260,16 @@ From the `harpoon' buffer you can rearrange and/or delete harpoons by swapping o
 	  (harpoon--insert-help (project-name proj))
 	  (goto-char (point-min))
 	  (unless (memq 'harpoon--process-buffer kill-buffer-hook)
-	    (add-hook 'kill-buffer-hook 'harpoon--process-buffer nil t))) 
+	    (add-hook 'kill-buffer-hook 'harpoon--process-buffer nil t)))
 	(pop-to-buffer buffer))
     (message "You either aren't in a project or there are no harpoons defined.")))
 
-(defun harpoon--write-file (file data)
-  (with-temp-file file
-    (prin1 data (current-buffer))))
-
-(defun harpoon--read-file (file)
-  (with-temp-buffer
-    (insert-file-contents file)
-    (goto-char (point-min))
-    (read (current-buffer))))
+(defun harpoon-process-buffer-and-quit ()
+  "Helper function to call `harpoon--process-buffer' and close the harpoon buffer."
+  (interactive)
+  (when-let ((buffer (get-buffer harpoon-buffer)))
+    (harpoon--process-buffer)
+    (kill-buffer buffer)))
 
 (defun harpoon-file-save ()
   "Save the global harpoon hash table to `harpoon-list-file'.
@@ -193,29 +278,6 @@ If the hash table is empty don't do anything."
   (interactive)
   (unless (eq (hash-table-count harpoon-global-list) 0)
     (harpoon--write-file harpoon-list-file harpoon-global-list)))
-
-(defun harpoon--file-load ()
-  (when (file-exists-p harpoon-list-file)
-    (let ((contents (harpoon--read-file harpoon-list-file)))
-      (setq harpoon-global-list contents)
-      (message "Harpoons loaded from file!"))))
-
-;;;###autoload
-(define-minor-mode harpoon-minor-mode
-  "A minor mode for using harpoon with emacs."
-  :lighter " Hp"
-  :keymap (let ((map (make-sparse-keymap)))
-	    map)
-  :global t
-  (if harpoon-minor-mode
-      (progn 
-	(harpoon--file-load)
-	(add-hook 'buffer-list-update-hook #'harpoon--retrieve)
-	(add-hook 'kill-emacs-hook #'harpoon-file-save))
-    (progn 
-      (harpoon-file-save)
-      (remove-hook 'buffer-list-update-hook #'harpoon--retrieve)
-      (remove-hook 'kill-emacs-hook #'harpoon-file-save))))
 
 (provide 'harpoon)
 ;;; harpoon.el ends here
